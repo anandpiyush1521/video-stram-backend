4a767b7f1fc1a8eaffd11fbe02bcf7166de12f4e
Piyush Anand <piyushanand2580@gmail.com>
2024-08-04 11:09:24 +0530

    repository and service added

---
+++ src/main/java/com/stream/app/services/VideoService.java
@@ -0,0 +1,14 @@
+package com.stream.app.services;
+
+import com.stream.app.entities.Video;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface VideoService {
+    Video save(Video video, MultipartFile file);
+    Video get(String videoId);
+    Video getByTitle(String title);
+    List<Video> getAll();
+
+}
